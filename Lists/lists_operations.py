ac5d62253d77ed56a8240e09d400d02481403932
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2024-08-27 00:28:13 +0600

    listed changed

--- Lists/lists_operations.py
+++ Lists/lists_operations.py
@@ -1,17 +1,19 @@
 # How List operations can be performed:
 
+# Creating a List: 
 List = ["a", "b", "c"]
 print(List)
 
+# Output:  ['a', 'b', 'c']
+
 # How to print the length of a list:
 print(len(List))
+
+# Output: 3
 
 # List can contain different datatypes:
 List_1 = ["abc", 30, True, "gf"]
 print(List_1)
 
-"""
-Output: ['a', 'b', 'c']
-        3
-        ['abc', 30, True, 'gf']
-"""
+# Output:  ['abc', 30, True, 'gf']
+
