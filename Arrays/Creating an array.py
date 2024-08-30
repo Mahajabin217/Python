25d80434e8d6de136509240dab4152c3ab6cdfa0
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2024-08-30 23:58:59 +0600

    array fixed

---
+++ Arrays/Creating an array.py
@@ -0,0 +1,15 @@
+# How To Create An Array:
+
+import array
+
+# Creating an Array of intergers numbers:
+int_ar = array.array('i',[1,2,3])
+print(int_ar)
+
+# Output: array('i', [1, 2, 3])
+
+# Creating an Array of floating numbers:
+float_ar = array.array('f',[1.1,1.2,1.3])
+print(float_ar)
+
+# Output: array('f', [1.100000023841858, 1.2000000476837158, 1.2999999523162842])
